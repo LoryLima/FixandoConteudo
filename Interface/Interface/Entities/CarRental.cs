13aac9b0bdb6abd655816104a69dd0e448eddb3d
Lorena <97991607+LoryLima@users.noreply.github.com>
2023-01-04 20:31:10 -0300

    nivel

---
+++ Interface/Interface/Entities/CarRental.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface.Entities
+{
+    internal class CarRental
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime star, DateTime finish, Vehicle vehicle)
+        {
+            Start = star;
+            Finish = finish;
+            Vehicle = vehicle;
+        }
+    }
+}
---
+++ Interface/Interface/Services/BrazilTaxService.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Interface.Services
+{
+    internal class BrazilTaxService : ITaxService // é da mesma forma da herança, mas não é herança, é uma realização de interface
+    {
+        public double Tax(double amount)
+        {
+            if (amount <= 100.0)
+            {
+                return amount * 0.2;
+            }
+            else
+            {
+                return amount * 0.15;
+            }
+        }
+    }
+}
