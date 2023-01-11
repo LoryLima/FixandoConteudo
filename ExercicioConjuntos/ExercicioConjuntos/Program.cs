f3cefbd72fbb396768c01f0951cee0cc537ff8a0
Lorena <97991607+LoryLima@users.noreply.github.com>
2023-01-11 18:52:08 -0300

    ConjuntosSet

---
+++ ExercicioConjuntos/ExercicioConjuntos/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+using ExercicioConjuntos.Entities;
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(' ');
+            string name = line[0];
+            DateTime instant = DateTime.Parse(line[1]);
+            set.Add(new LogRecord(name, instant));
+        }
+        Console.WriteLine("Total users: " + set.Count);
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
