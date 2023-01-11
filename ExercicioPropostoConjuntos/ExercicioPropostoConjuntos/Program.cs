f8a83be0710eace763516ac0500ab7fa2d707f15
Lorena <97991607+LoryLima@users.noreply.github.com>
2023-01-11 19:36:35 -0300

    Exercicio Proposto

---
+++ ExercicioPropostoConjuntos/ExercicioPropostoConjuntos/Program.cs
@@ -0,0 +1,37 @@
+﻿using ExercicioPropostoConjuntos;
+
+
+HashSet<Student> alunos = new HashSet<Student>();
+
+Console.WriteLine("Quantos alunos tem no curso A: ");
+int alunosCursoA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre com o código dos alunos: ");
+for (int i = 1; i <= alunosCursoA; i++)
+{
+    int idA = int.Parse(Console.ReadLine());
+    alunos.Add(new Student(idA)); 
+}
+
+Console.WriteLine("Quantos alunos tem no curso A: ");
+int alunosCursoB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre com o código dos alunos: ");
+for (int i = 1; i <= alunosCursoB; i++)
+{
+    int idB = int.Parse(Console.ReadLine());
+    alunos.Add(new Student(idB));
+}
+
+Console.WriteLine("Quantos alunos tem no curso C: ");
+int alunosCursoC = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre com o código dos alunos: ");
+for (int i = 1; i <= alunosCursoC; i++)
+{
+    int idC = int.Parse(Console.ReadLine());
+    alunos.Add(new Student(idC));
+}
+
+Console.WriteLine("Total de estudantes " + alunos.Count);
+
---
+++ ExercicioPropostoConjuntos/ExercicioPropostoConjuntos/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPropostoConjuntos
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+
+        public Student(int id)
+        {
+            Id = id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Student))
+            {
+                return false;
+            }
+
+            Student other = obj as Student;
+                
+            return Id.Equals(other.Id);
+        }
+
+        public override string ToString()
+        {
+            return Id.ToString();
+        }
+    }
+}
